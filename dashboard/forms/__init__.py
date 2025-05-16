82ee4c61e0758db25f62b065b4d340a7a9f40983
Ethan Betts <you@example.com>
2025-05-16 14:33:06 +0200

    checkpoint

--- dashboard/forms/__init__.py
+++ dashboard/forms/__init__.py
@@ -1,9 +1,25 @@
+# dashboard/forms/__init__.py
+
+# Import all your forms here so they can be imported directly from dashboard.forms
 from .about_page_content_form import AboutPageContentForm
-from .business_info_form import BusinessInfoForm
-from .visibility_settings_form import VisibilitySettingsForm
-from .service_booking_settings_form import ServiceBookingSettingsForm
-from .hire_booking_settings_form import HireBookingSettingsForm
-from .service_type_form import ServiceTypeForm
+from .blocked_hire_date_form import BlockedHireDateForm
 from .blocked_service_date_form import BlockedServiceDateForm
+from .business_info_form import BusinessInfoForm
+from .hire_booking_settings_form import HireBookingSettingsForm
+from .service_booking_settings_form import ServiceBookingSettingsForm
 from .service_brand_form import ServiceBrandForm
-from .blocked_hire_date_form import BlockedHireDate
+from .service_type_form import ServiceTypeForm
+from .visibility_settings_form import VisibilitySettingsForm
+
+# You can also define __all__ to explicitly list what is exported
+__all__ = [
+    'AboutPageContentForm',
+    'BlockedHireDateForm',
+    'BlockedServiceDateForm',
+    'BusinessInfoForm',
+    'HireBookingSettingsForm',
+    'ServiceBookingSettingsForm',
+    'ServiceBrandForm',
+    'ServiceTypeForm',
+    'VisibilitySettingsForm',
+]
