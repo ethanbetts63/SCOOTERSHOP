734f7977b14f1e04e9dece322d10bb0d4511b878
Ethan Betts <you@example.com>
2025-05-16 14:07:00 +0200

    migrated

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -4,3 +4,4 @@ from .about_page_content import AboutPageContent
 from .blocked_service_date import BlockedServiceDate
 from .service_brand import ServiceBrand
 from .hire_settings import HireSettings
+from .blocked_hire_date import BlockedHireDate
