b8c47f71692d4901d653abf5657b9f9c8830f36c
Ethan Betts <you@example.com>
2025-06-12 17:42:51 +0200

    checkpoint

---
+++ inventory/tests/form_tests/test_admin_motorcycle_image_form.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django import forms
+from django.forms.models import BaseModelFormSet
+from inventory.models import Motorcycle, MotorcycleImage
+from inventory.forms.admin_motorcycle_image_form import MotorcycleImageForm, MotorcycleImageFormSet
+from ..test_helpers.model_factories import MotorcycleFactory, MotorcycleImageFactory
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+class MotorcycleImageFormTest(TestCase):
+
+    def test_form_fields_and_widgets(self):
+        form = MotorcycleImageForm()
+        expected_fields = ['image']
+        self.assertEqual(list(form.fields.keys()), expected_fields)
+        self.assertIsInstance(form.fields['image'].widget, forms.FileInput)
+        self.assertIn('form-control', form.fields['image'].widget.attrs['class'])
+
+    def test_form_with_valid_image_data(self):
+        image_content = b"fake_image_data"
+        image_file = SimpleUploadedFile("test_image.jpg", image_content, content_type="image/jpeg")
+        data = {'image': image_file}
+        form = MotorcycleImageForm(files=data)
+        self.assertTrue(form.is_valid(), str(form.errors))
+        motorcycle_image = form.save(commit=False)
+        self.assertEqual(motorcycle_image.image.read(), image_content)
+
+    def test_form_without_image_data(self):
+        form = MotorcycleImageForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertIn('image', form.errors)
+        self.assertEqual(form.errors['image'], ['This field is required.'])
+
+
