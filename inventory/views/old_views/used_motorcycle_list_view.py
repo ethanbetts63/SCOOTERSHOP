6d8c7f42a78fc1212653bf9b15a01117ec4dc8bf
Ethan Betts <you@example.com>
2025-06-14 15:01:23 +0200

    checkpoint

--- inventory/views/old_views/used_motorcycle_list_view.py
+++
@@ -1,8 +0,0 @@
-# # inventory/views/used_motorcycle_list_view.py
-
-# from .motorcycle_list_view import MotorcycleListView 
-
-# class UsedMotorcycleListView(MotorcycleListView):
-#     template_name = 'inventory/used.html'
-#     condition_name = 'used'
-#     url_name = 'inventory:used'
