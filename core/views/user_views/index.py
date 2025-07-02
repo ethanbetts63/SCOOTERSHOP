f8ae5368179babfe1691ed0ed8a58c4a31e6f358
Ethan Betts <you@example.com>
2025-07-02 21:20:39 +0200

    checkpoint

--- core/views/user_views/index.py
+++ core/views/user_views/index.py
@@ -7,6 +7,7 @@ from service.models import ServiceSettings, TempServiceBooking
 from service.forms import ServiceDetailsForm
 from service.utils import get_service_date_availability
 from django.views.decorators.http import require_http_methods
+from inventory.utils import get_featured_motorcycles # --- Import the utility ---
 
 
 @require_http_methods(["GET"])
@@ -68,6 +69,9 @@ def index(request):
 
     min_date_for_flatpickr, disabled_dates_json = get_service_date_availability()
 
+    featured_new_items = get_featured_motorcycles('new')
+    featured_used_items = get_featured_motorcycles('used')
+
     context = {
         "reviews": five_star_reviews,
         "google_api_key": settings.GOOGLE_API_KEY,
@@ -76,6 +80,8 @@ def index(request):
         "blocked_service_dates_json": disabled_dates_json,
         "min_service_date_flatpickr": min_date_for_flatpickr.strftime("%Y-%m-%d"),
         "temp_service_booking": temp_service_booking,
+        "featured_new_items": featured_new_items,
+        "featured_used_items": featured_used_items,
     }
 
     return render(request, "core/index.html", context)
