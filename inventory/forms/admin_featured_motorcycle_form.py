c868a656a8b13b121339748abcb3febeda971df5
Ethan Betts <you@example.com>
2025-07-02 20:08:46 +0200

    checkpoint

--- inventory/forms/admin_featured_motorcycle_form.py
+++ inventory/forms/admin_featured_motorcycle_form.py
@@ -1,21 +1,30 @@
 from django import forms
-from inventory.models import FeaturedMotorcycle, Motorcycle
+from inventory.models import FeaturedMotorcycle
 
 class FeaturedMotorcycleForm(forms.ModelForm):
     class Meta:
         model = FeaturedMotorcycle
         fields = ['motorcycle', 'category', 'order']
+        widgets = {
+            'motorcycle': forms.HiddenInput(),
+            'category': forms.HiddenInput(),
+        }
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes the form. No special queryset filtering is needed anymore
+        as the motorcycle is selected via an AJAX search.
+        """
         super().__init__(*args, **kwargs)
-        if self.instance and self.instance.pk:
-            if self.instance.category == 'new':
-                self.fields['motorcycle'].queryset = Motorcycle.objects.filter(condition='new')
-            elif self.instance.category == 'used':
-                self.fields['motorcycle'].queryset = Motorcycle.objects.filter(condition='used')
-        else:
-            category = self.initial.get('category')
-            if category == 'new':
-                self.fields['motorcycle'].queryset = Motorcycle.objects.filter(condition='new')
-            elif category == 'used':
-                self.fields['motorcycle'].queryset = Motorcycle.objects.filter(condition='used')
+        # The 'order' field can be styled or have attributes set here if needed
+        self.fields['order'].widget.attrs.update({'class': 'form-control w-full p-2 border rounded-md'})
+
+    def clean_motorcycle(self):
+        """
+        Custom validation for the motorcycle field to ensure it's not empty.
+        """
+        motorcycle = self.cleaned_data.get('motorcycle')
+        if not motorcycle:
+            raise forms.ValidationError("You must select a motorcycle.")
+        return motorcycle
+
--- inventory/views/admin_views/featured_motorcycle_create_update.py
+++ inventory/views/admin_views/featured_motorcycle_create_update.py
@@ -1,8 +1,8 @@
 from django.views.generic import UpdateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
-from inventory.models import FeaturedMotorcycle
+from inventory.models import FeaturedMotorcycle, Motorcycle
 from inventory.forms.admin_featured_motorcycle_form import FeaturedMotorcycleForm
 
 class FeaturedMotorcycleCreateUpdateView(UpdateView):
@@ -12,12 +12,20 @@ class FeaturedMotorcycleCreateUpdateView(UpdateView):
     context_object_name = "featured_motorcycle"
 
     def get_object(self, queryset=None):
+        """
+        Returns the FeaturedMotorcycle instance that the view is displaying.
+        For new instances, returns None.
+        """
         pk = self.kwargs.get("pk")
         if pk:
             return get_object_or_404(FeaturedMotorcycle, pk=pk)
         return None
 
     def get_initial(self):
+        """
+        Returns the initial data to use for forms on this view.
+        Sets the 'category' from the URL parameter if present.
+        """
         initial = super().get_initial()
         category = self.request.GET.get('category')
         if category:
@@ -25,15 +33,40 @@ class FeaturedMotorcycleCreateUpdateView(UpdateView):
         return initial
 
     def get_context_data(self, **kwargs):
+        """
+        Inserts additional context into the template.
+        """
         context = super().get_context_data(**kwargs)
+        self.object = self.get_object()
+
+        # Determine the motorcycle condition ('new' or 'used')
+        motorcycle_condition = self.request.GET.get('category')
+        if self.object:
+            motorcycle_condition = self.object.category
+        
         context["title"] = (
-            f"Edit Featured Motorcycle: {self.object.motorcycle.title}"
+            f"Edit Featured {motorcycle_condition.title()} Motorcycle"
             if self.object
-            else "Add New Featured Motorcycle"
+            else f"Add New Featured {motorcycle_condition.title()} Motorcycle"
         )
+        context["motorcycle_condition"] = motorcycle_condition
+        
+        # Pass the currently selected motorcycle if editing
+        if self.object:
+            context["selected_motorcycle"] = self.object.motorcycle
+
+        # URLs for AJAX calls in the template
+        context["ajax_search_motorcycles_url"] = reverse("inventory:admin_api_search_motorcycles")
+        context["ajax_get_motorcycle_details_url"] = reverse("inventory:admin_api_get_motorcycle_details", kwargs={'pk': 0}).replace('0', '')
+
+
         return context
 
     def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance with the passed
+        POST variables and then validating it.
+        """
         self.object = self.get_object()
         form = self.get_form()
 
@@ -42,8 +75,12 @@ class FeaturedMotorcycleCreateUpdateView(UpdateView):
             messages.success(self.request, "Featured motorcycle saved successfully!")
             return redirect(self.get_success_url())
         else:
+            # Re-render the form with validation errors.
             messages.error(self.request, "Please correct the errors below.")
             return self.render_to_response(self.get_context_data(form=form))
 
     def get_success_url(self):
+        """
+        Returns the URL to redirect to after processing a valid form.
+        """
         return reverse_lazy("inventory:featured_motorcycles")
