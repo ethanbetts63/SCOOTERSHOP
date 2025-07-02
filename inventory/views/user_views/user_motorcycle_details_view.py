aeb635cbaf0bdbfe816c2ca78dc8a2216dfdbb4f
Ethan Betts <you@example.com>
2025-07-02 20:54:18 +0200

    checkpoint

--- inventory/views/user_views/user_motorcycle_details_view.py
+++ inventory/views/user_views/user_motorcycle_details_view.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from inventory.models import Motorcycle, InventorySettings
 from inventory.utils.get_motorcycle_details import get_motorcycle_details
 from inventory.utils.get_sales_faqs import get_faqs_for_step
+from inventory.utils import get_featured_motorcycles
 
 
 class UserMotorcycleDetailsView(DetailView):
@@ -20,10 +21,21 @@ class UserMotorcycleDetailsView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        motorcycle = self.object  
+        
         inventory_settings = InventorySettings.objects.first()
         context["inventory_settings"] = inventory_settings
 
         context["sales_faqs"] = get_faqs_for_step("general")
         context["faq_title"] = "Questions About Our Motorcycles"
+
+        category = motorcycle.condition
+        if category in ['new', 'used']:
+            featured_items = get_featured_motorcycles(category)
+            featured_items = featured_items.exclude(motorcycle=motorcycle)
+            
+            context['featured_items'] = featured_items
+            context['section_title'] = f"Other Featured {category.title()} Motorcycles"
+            context['category'] = category
 
         return context
