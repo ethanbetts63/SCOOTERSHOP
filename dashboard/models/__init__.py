253385e7faaceb634e8fa4c90c77e3027f4865a9
Ethan Betts <you@example.com>
2025-07-03 16:06:09 +0200

    checkpoint

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -1,3 +1,1 @@
 from .site_settings import SiteSettings
-from .page_content_base import PageContentBase
-from .about_page_content import AboutPageContent
--- dashboard/models/page_content_base.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-                             
-class PageContentBase(models.Model):
-    """Base model for page content"""
-    last_updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True 
