51ad1b9324606dcea9243f82617cc4369269f1d4
Ethan Betts <you@example.com>
2025-07-04 18:07:07 +0200

    checkpoint

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,21 +1,19 @@
-from .models import VisibilitySettings, BusinessInfo
+from .models import SiteSettings
 from django.conf import settings
 from service.models import ServiceType
                                 
 def site_settings(request):
-    visibility_settings = None
-    business_info_settings = None
+    settings_object = None
     service_types_list = []
 
     try:
-        visibility_settings = VisibilitySettings.get_settings()
-        business_info_settings = BusinessInfo.get_settings()
-        service_types_list = ServiceType.objects.all()
+        settings_object = SiteSettings.get_settings()
+        if settings_object:
+            service_types_list = ServiceType.objects.all()
     except Exception:                                              
         pass                   
 
     return {
-        'visibility_settings': visibility_settings,
-        'business_info_settings': business_info_settings,
+        'settings': settings_object, 
         'service_types': service_types_list,
     }
