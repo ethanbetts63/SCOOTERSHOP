a3886ff3897a4de221c0e0335ce79a9fc5753727
Ethan Betts <you@example.com>
2025-07-07 16:46:44 +0200

    checkpoint

--- mailer/views/admin_views/email_details_view.py
+++ mailer/views/admin_views/email_details_view.py
@@ -1,8 +1,7 @@
 from django.views.generic import DetailView
 from mailer.models import EmailLog
 from core.mixins import AdminRequiredMixin
-from django.template import Context, Template
-from django.shortcuts import render
+import base64
 
 class EmailDetailView(AdminRequiredMixin, DetailView):
     model = EmailLog
@@ -13,5 +12,12 @@ class EmailDetailView(AdminRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context["page_title"] = "Email Details"
         email_log = self.get_object()
-        context['rendered_email'] = email_log.html_content
+        
+        # Encode the HTML content in base64 to use in a data URL
+        if email_log.html_content:
+            encoded_html = base64.b64encode(email_log.html_content.encode('utf-8')).decode('utf-8')
+            context['encoded_html_content'] = encoded_html
+        else:
+            context['encoded_html_content'] = base64.b64encode(b'<p>No content available.</p>').decode('utf-8')
+            
         return context
