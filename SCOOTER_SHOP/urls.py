37ede1312f8df9b70a78df1419437d9e09e70691
Ethan Betts <you@example.com>
2025-07-10 13:12:24 +0200

    checkpoint

--- SCOOTER_SHOP/urls.py
+++ SCOOTER_SHOP/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,7 +13,15 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
     path('', include('core.urls', namespace='core')),
     path('payments/', include('payments.urls', namespace="payments")),
-    path('mailer/', include('mailer.urls', namespace='mailer'))
+    path('mailer/', include('mailer.urls', namespace='mailer')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+    path(
+        "sitemap.xml",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml"),
+    ),
 ]
 
 if settings.DEBUG:
