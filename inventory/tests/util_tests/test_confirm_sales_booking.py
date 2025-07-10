d394a083ef04c40c5f715befe01cc15060e9acb3
Ethan Betts <you@example.com>
2025-07-10 11:39:29 +0200

    checkpoint

--- inventory/tests/util_tests/test_confirm_sales_booking.py
+++ inventory/tests/util_tests/test_confirm_sales_booking.py
@@ -38,10 +38,8 @@ class ConfirmSalesBookingTests(TestCase):
 
     @patch('inventory.utils.confirm_sales_booking.send_templated_email')
     def test_confirm_sales_booking_used_motorcycle(self, mock_send_email):
-        used_motorcycle = MotorcycleFactory(quantity=1, condition='used', is_available=True, status='available')
+        used_motorcycle = MotorcycleFactory(condition='used', status='available')
         used_booking = SalesBookingFactory(motorcycle=used_motorcycle)
-        used_motorcycle.condition = 'used'
-        used_motorcycle.save()
         result = confirm_sales_booking(used_booking.id)
         self.assertTrue(result['success'])
         used_booking.refresh_from_db()
