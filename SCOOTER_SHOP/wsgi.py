2866f2163132e1a14873f6a620ea9a2875410dcc
Ethan Betts <you@example.com>
2025-07-16 13:05:07 +0200

    checkpoint

--- SCOOTER_SHOP/wsgi.py
+++ SCOOTER_SHOP/wsgi.py
@@ -1,5 +1,3 @@
-# local version
-
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
@@ -10,20 +8,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SCOOTER_SHOP.settings")
 
 application = get_wsgi_application()
 
-
-# python anywhere version
-import os
-import sys
-from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-
-project_folder = os.path.expanduser('~/ScooterShop')
-load_dotenv(os.path.join(project_folder, '.env'))
-
-path = '/home/ethanbetts/ScooterShop'  
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SCOOTER_SHOP.settings')
-
-application = get_wsgi_application()
