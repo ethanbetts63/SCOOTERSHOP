6acabca3bf0d6c6a45ac8cee0f9bc9b4d16cbe8c
Ethan Betts <you@example.com>
2025-07-19 13:14:16 +0200

    checkpoint

--- refunds/tests/view_tests/admin_view_tests/test_admin_edit_refund_request_view.py
+++ refunds/tests/view_tests/admin_view_tests/test_admin_edit_refund_request_view.py
@@ -4,14 +4,16 @@ from django.urls import reverse
 from django.contrib.messages import get_messages
 from users.tests.test_helpers.model_factories import UserFactory
 from refunds.tests.test_helpers.model_factories import RefundRequestFactory
+from service.tests.test_helpers.model_factories import ServiceBookingFactory
 
 class AdminEditRefundRequestViewTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = UserFactory(is_staff=True, is_superuser=True)
         self.client.force_login(self.admin_user)
-        self.refund_request = RefundRequestFactory()
-        self.edit_url = reverse("refunds:admin_edit_refund_request", kwargs={"pk": self.refund_request.pk})
+        self.service_booking = ServiceBookingFactory(payment__status='succeeded')
+        self.refund_request = RefundRequestFactory(service_booking=self.service_booking)
+        self.edit_url = reverse("refunds:edit_refund_request", kwargs={"pk": self.refund_request.pk})
 
     def test_get_edit_refund_request_view(self):
         response = self.client.get(self.edit_url)
@@ -21,6 +23,7 @@ class AdminEditRefundRequestViewTest(TestCase):
 
     def test_edit_refund_request_successfully(self):
         form_data = {
+            "booking_reference": self.service_booking.service_booking_reference,
             "reason": "Updated reason",
             "amount_to_refund": "150.00",
             "status": "approved",
