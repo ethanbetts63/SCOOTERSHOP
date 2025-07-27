0f19efbc85174df8c3536f71005bf2977ffb1889
Ethan Betts <you@example.com>
2025-07-27 14:13:15 +0200

    checkpoint

--- service/views/admin_views/delete_service_type.py
+++ service/views/admin_views/delete_service_type.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.urls import reverse
 from django.contrib import messages
 from service.mixins import AdminRequiredMixin
+from django.db.models import ProtectedError
 
 from service.models import ServiceType
 
@@ -14,8 +15,14 @@ class ServiceTypeDeleteView(AdminRequiredMixin, View):
     def post(self, request, pk, *args, **kwargs):
         service_type = get_object_or_404(ServiceType, pk=pk)
         service_type_name = service_type.name
-        service_type.delete()
-        messages.success(
-            request, f"Service Type '{service_type_name}' deleted successfully."
-        )
+        try:
+            service_type.delete()
+            messages.success(
+                request, f"Service Type '{service_type_name}' deleted successfully."
+            )
+        except ProtectedError:
+            messages.error(
+                request,
+                f"Cannot delete '{service_type_name}' because it is associated with existing service bookings.",
+            )
         return redirect(reverse("service:service_types_management"))
